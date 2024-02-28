4aaa0d76afd6542ef3880e6abb339d9f736d73d3
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-02-28 15:21:41 -0300

    Roles Enum

---
+++ src/main/java/com/redue/newsflow/enums/Roles.java
@@ -0,0 +1,15 @@
+package com.redue.newsflow.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Roles {
+    OWNER("ROLE_OWNER"),
+    USER("ROLE_USER");
+
+    private final String role;
+
+    Roles(String role) {
+        this.role = role;
+    }
+}
