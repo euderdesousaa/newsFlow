31995548431607915c1ea4e09e654ed1529bc29e
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-02-28 15:22:50 -0300

    UserRepository

---
+++ src/main/java/com/redue/newsflow/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.redue.newsflow.repositories;
+
+import com.redue.newsflow.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByUsernameOrEmail(String username, String email);
+}
