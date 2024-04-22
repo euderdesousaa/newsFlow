5dd594e394039f5787fd917644c6ded6a6456372
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-04-22 10:16:15 -0300

    user

--- src/main/java/com/redue/newsflow/entities/User.java
+++ src/main/java/com/redue/newsflow/entities/User.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Getter
 @Setter
@@ -22,6 +21,7 @@ public class User {
     private String id;
 
     private String username;
+
     private String name;
 
     @Email
