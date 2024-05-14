17c5cf84a0891823d30a0e8894ddb40bbc2c9ea3
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-05-14 11:49:47 -0300

    String Country

--- src/main/java/com/redue/newsflow/dto/SignUpDto.java
+++ src/main/java/com/redue/newsflow/dto/SignUpDto.java
@@ -19,5 +19,7 @@ public record SignUpDto(
         @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$",
                 message = "password must be min 4 and max 12 length containing at least 1 uppercase, 1 lowercase, 1 special character and 1 digit ")
         @NotBlank
-        String password) {
+        String password,
+
+        String Country) {
 }
