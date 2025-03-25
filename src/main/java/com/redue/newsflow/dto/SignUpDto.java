7c63f582975852dba8167ba274f71df4a2560a0a
Euder de Sousa <euderdesousaaaa@gmail.com>
2025-03-25 16:36:05 -0300

    I don't need Country in SignUpDto, that's the responsibility of other classes

--- src/main/java/com/redue/newsflow/dto/SignUpDto.java
+++ src/main/java/com/redue/newsflow/dto/SignUpDto.java
@@ -2,7 +2,6 @@ package com.redue.newsflow.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 
 public record SignUpDto(
 
@@ -19,7 +18,5 @@ public record SignUpDto(
         // @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$",
        //         message = "password must be min 4 and max 12 length containing at least 1 uppercase, 1 lowercase, 1 special character and 1 digit ")
         @NotBlank
-        String password,
-
-        String Country) {
+        String password) {
 }
