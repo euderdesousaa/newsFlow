c5885af0357ae3cc30f2434accba474c60fd2064
Euder de Sousa <euderdesousaaaa@gmail.com>
2025-04-01 20:38:57 -0300

    Adding annotation for enable Cross Origin from any sources.

--- src/main/java/com/redue/newsflow/controller/TestController.java
+++ src/main/java/com/redue/newsflow/controller/TestController.java
@@ -2,25 +2,22 @@ package com.redue.newsflow.controller;
 
 import com.redue.newsflow.dto.NewsArticle;
 import com.redue.newsflow.dto.NewsDTO;
-import com.redue.newsflow.service.ScrappingService;
+import com.redue.newsflow.service.NewsScrappingService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController()
 @RequestMapping("/api/v1/test")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class TestController {
-
-
-    private final ScrappingService scrappingService;
+    
+    private final NewsScrappingService scrappingService;
 
     @GetMapping
     public List<NewsDTO> fetchLastNews() {
@@ -32,10 +29,10 @@ public class TestController {
                                         @RequestParam(required = false) String category, HttpServletRequest request) {
         HttpSession session = request.getSession();
         String userCountry = (String) session.getAttribute("userCountry");
-        System.out.println(userCountry);
         return scrappingService.scrapeAllNews(source, category, userCountry);
-    }
-
+    } 
+    
+    //TEST.
     @GetMapping("/get-country")
     public ResponseEntity<String> getUserCountry(HttpServletRequest request) {
         HttpSession session = request.getSession();
