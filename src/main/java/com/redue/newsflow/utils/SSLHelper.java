123129fe89fff9bf3cc1d796dc691b40790915b2
Euder de Sousa <euderdesousaaaa@gmail.com>
2025-04-02 22:03:37 -0300

    SSLHelper

---
+++ src/main/java/com/redue/newsflow/utils/SSLHelper.java
@@ -0,0 +1,34 @@
+package com.redue.newsflow.utils;
+
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
+
+public class SSLHelper {
+    public static void disableSSLCertificateValidation() {
+        try {
+            TrustManager[] trustAllCertificates = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCertificates, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Ignora hostname inválido
+            HostnameVerifier allHostsValid = (hostname, session) -> true;
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
