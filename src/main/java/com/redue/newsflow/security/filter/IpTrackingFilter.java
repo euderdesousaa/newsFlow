efa34cb4f2c0905dd979c1dd8f9c468c7e3dbb22
Euder de Sousa <euderdesousaaaa@gmail.com>
2025-04-08 15:25:00 -0300

    We are now capturing IP and City in the filter.

--- src/main/java/com/redue/newsflow/security/filter/IpTrackingFilter.java
+++ src/main/java/com/redue/newsflow/security/filter/IpTrackingFilter.java
@@ -14,12 +14,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 @Component
 @WebFilter("/*")
 public class IpTrackingFilter implements Filter {
-    
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
@@ -33,41 +35,54 @@ public class IpTrackingFilter implements Filter {
             System.out.println(ipAddress);
         }
 
-        // Verifica se já temos o ISO Code salvo na sessão
         String isoCode = (String) session.getAttribute("userCountry");
-        if (isoCode == null) {
-            isoCode = getCountryIsoCode(ipAddress); 
+        String city = (String) session.getAttribute("userCity");
+
+        if (isoCode == null || city == null) {
+            Map<String, String> locationData = getLocationData(ipAddress);
+            isoCode = locationData.get("country");
+            city = locationData.get("city");
+
             session.setAttribute("userCountry", isoCode);
+            session.setAttribute("userCity", city);
         }
 
         chain.doFilter(request, response);
     }
 
-    private String getCountryIsoCode(String ip) {
+    private Map<String, String> getLocationData(String ip) {
+        Map<String, String> location = new HashMap<>();
         try {
-            String apiUrl = "https://ipinfo.io/" + "45.234.138.189" + "/json";
-            //US: 65.29.148.75
-            //BR: 45.234.138.189
-            URL url = new URL(apiUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String inputLine;
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            JSONObject json = new JSONObject(response.toString());
-            return json.getString("country");
+            String apiUrl = "https://ipinfo.io/" + ip + "/json";
+            
+            JSONObject json = getJsonObject(apiUrl);
+            location.put("city", json.getString("city"));
+            location.put("country", json.getString("country"));
 
         } catch (Exception e) {
             e.printStackTrace();
-            return "Unknown";
+            location.put("city", "Unknown");
+            location.put("country", "Unknown");
         }
+        return location;
+    }
+
+    private static JSONObject getJsonObject(String apiUrl) throws IOException {
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        JSONObject json = new JSONObject(response.toString());
+        return json;
     }
 }
